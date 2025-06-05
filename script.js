7d958ee6414833d6a4afdcc7d5fd3bedd40d61f7
Mafer0610 <maria.cruz74@unach.mx~>
2025-06-05 15:45:45 -0600

    base

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCszb01ppFx3-lvMGYHjBNgEh_5oi3UXkA",
+    authDomain: "proyecto-d910f.firebaseapp.com",
+    projectId: "proyecto-d910f",
+    storageBucket: "proyecto-d910f.firebasestorage.app",
+    messagingSenderId: "159534098700",
+    appId: "1:159534098700:web:2757ec1bb82fdb44818129"
+};
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
 function iniciarSesion() {
-    const usuarioIngresado = document.getElementById("username").value.trim();
+    const emailIngresado = document.getElementById("email").value.trim();
     const contrasenaIngresada = document.getElementById("password").value.trim();
     const mensaje = document.getElementById("mensajeLogin");
 
-    db.collection("usuarios")
-        .where("usuario", "==", usuarioIngresado)
-        .where("password", "==", contrasenaIngresada)
-        .get()
-        .then((querySnapshot) => {
-        if (!querySnapshot.empty) {
+    auth.signInWithEmailAndPassword(emailIngresado, contrasenaIngresada)
+        .then((userCredential) => {
             mensaje.textContent = "Ingreso exitoso. Redirigiendo...";
             mensaje.style.color = "green";
-
             setTimeout(() => {
-            window.location.href = "index.html";
+                window.location.href = "index.html";
             }, 1000);
-        } else {
-            mensaje.textContent = "Usuario o contraseña incorrectos";
-            mensaje.style.color = "red";
-        }
         })
         .catch((error) => {
-        console.error("Error al iniciar sesión:", error);
-        mensaje.textContent = "Error al intentar iniciar sesión";
-        mensaje.style.color = "orange";
+            mensaje.textContent = "Error: " + error.message;
+            mensaje.style.color = "red";
         });
 }
